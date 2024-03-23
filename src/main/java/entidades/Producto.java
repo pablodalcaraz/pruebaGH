1991c41ed49ab007dc1e7c76b04d9b25d337bbde
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-03-23 17:35:11 -0300

    agrego atributos a la clase producto

--- src/main/java/entidades/Producto.java
+++ src/main/java/entidades/Producto.java
@@ -9,5 +9,39 @@ package entidades;
  * @author Pablo
  */
 public class Producto {
+    private int id;
+    private String descripcion;
+    private double precio;
+
+    public Producto(int id, String descripcion, double precio) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
     
 }
